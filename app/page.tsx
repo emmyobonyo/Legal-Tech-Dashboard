42321e19d089aa82933d4dad27454b4abe6d567e
Emmanuel Obonyo <emmyobonyo@gmail.com>
2025-03-25 02:15:03 +0300

    UI: implement Login UI

--- app/page.tsx
+++ app/page.tsx
@@ -20,5 +20,58 @@ export default function Home() {
   }, [dispatch]);
 
   useEffect(() => {}, []);
-  return <h1 className="text-xl font-medium underline">Hello world!</h1>;
+  return (
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+        >
+          Legal Tech DashBoard
+        </a>
+        <div className="w-full bg-white rounded-lg shadow-2xl md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-gray-600 text-md font-bold"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="name@company.com"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-mb font-bold text-gray-600 "
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                />
+              </div>
+              <button className="w-full text-white bg-gray-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:bg-gray-700 font-bold rounded-lg text-md px-5 py-2.5 text-center cursor-pointer">
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
